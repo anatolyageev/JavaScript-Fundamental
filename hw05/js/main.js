dd09f3df6ff1013dabf1078899ddfd9a43f1f375
Anatolii <anatol1812@gmail.com>
2020-05-21 23:34:10 +0300

    Added HW5

--- hw05/js/main.js
+++ hw05/js/main.js
@@ -30,7 +30,95 @@ function deMux(char) {
 console.log(DNA_strand("ATTGC"));
 console.log(DNA_strand("GTAT"));
 
-//Task2 
+//Task2.1
+
+let dozens = new Map();
+dozens.set(2, 'двадцать')
+    .set(3, 'тридцать')
+    .set(4, 'сорок')
+    .set(5, 'пятьдесят')
+    .set(6, 'шестьдесят')
+    .set(7, 'семьдесят')
+    .set(8, 'восемьдесят')
+    .set(9, 'девяносто');
+
+let units = new Map();
+units.set(1, 'один')
+    .set(2, 'два')
+    .set(3, 'три')
+    .set(4, 'четыре')
+    .set(5, 'пять')
+    .set(6, 'шесть')
+    .set(7, 'семь')
+    .set(8, 'восемь')
+    .set(9, 'девять');
+
+let digitsSecondDec = new Map();
+digitsSecondDec.set(10, 'десять').set(11, 'одинадцать')
+    .set(12, 'двенадцать')
+    .set(13, 'тринадцать')
+    .set(14, 'четырнадцать')
+    .set(15, 'пятнадцать')
+    .set(16, 'шестнадцать')
+    .set(17, 'семнадцать')
+    .set(18, 'восемнадцать')
+    .set(19, 'девятнадцать');
+
+
+function digitInWords(digit) {
+    if (digit < 20) {
+        return digitsSecondDec.get(digit);
+    }
+    let doz = ~~(digit / 10);
+    let un = digit % 10;
+    return dozens.get(doz) + " " + units.get(un);
+}
+
+//Task 2.2
+
+function stringInfo(str) {
+    let arr = str.split("");
+    console.log(arr);
+    let countChars = arr.reduce((result, el) => {
+        if (el.match(/[a-zA-Z]/)) {
+            result += 1;
+        }
+        return result;
+    }, 0);
+    let countDigits = arr.reduce((result, el) => {
+        if (el.match(/\d/)) {
+            result += 1;
+        }
+        return result;
+    }, 0);
+    return "Characters: " + countChars +
+        "\nDigits: " + countDigits +
+        "\nOther: " + (str.length - (countChars + countDigits));
+}
+console.log(stringInfo('som5e stri4ng!'));
+
+//Task 2.3
+function convertString(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(/\d/)) {
+            result += "_";
+        } else if (str[i] == str[i].toUpperCase()) {
+            result += str[i].toLowerCase();
+        } else if (str[i] == str[i].toLowerCase()) {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
+
+console.log(convertString('sOm5e7sI ri4Mg!'));
+
+//Task3
+
+
 
 
 
